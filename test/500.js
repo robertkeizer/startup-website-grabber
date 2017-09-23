f1b4222c71b505ae96a3cb77c295c3763b278be6
Robert Keizer <robert@keizer.ca>
2017-09-23 10:55:54 -0500

    Stupid travis.

--- test/500.js
+++ test/500.js
@@ -4,8 +4,9 @@ describe( "500", function( ){
 	it.skip( "Can return a list of websites (long)", function( cb ){
 
 		const fiveHundred = FiveHundred( );
-		let _links = [ ];
-		fiveHundred.on( "link", function( link ){ _links.push(link); } );
+
+		let links = [ ];
+		fiveHundred.on( "link", function( link ){ links.push(link); } );
 
 		fiveHundred.on( "done", function( ){
 			if( _links.length == 0 ){ return cb( "no links" ); }
